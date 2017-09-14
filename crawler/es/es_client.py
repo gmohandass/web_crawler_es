91787d08694f0b9c3434e7751a5ee299461ac91f
Girish <locategirish@gmail.com>
2017-09-13 20:38:15 -0700

    Added Comments and remote es host

--- crawler/es/es_client.py
+++ crawler/es/es_client.py
@@ -12,6 +12,19 @@ class ESClient(object):
         self.test_run = test_run
         self.index_name = "_".join(["crawler", author, "test"]) if test_run else "crawler"
 
+    """
+    This method can be used to download a webpage and index it into Elasticsearch.
+    
+    Args:
+        url: (String) This parameter expects the url or the page to be indexed in elasticsearch.
+        website_name: (String) This parameter expects the name of the website (eg. freelancer).
+    
+    Returns:
+        The response is directed from elasticsearch
+    
+    Raises:
+        KeyError: Raises an exception.
+    """
     def publish_doc(self, url, website_name):
 
         try:
@@ -38,6 +51,12 @@ class ESClient(object):
             raise e
 
 
+"""
+Below is an example of how to index a specific web page into elasticsearch.
+Elasticsearch url is http://34.230.32.193:9200.
+"""
 if __name__ == '__main__':
-    my_es = ESClient('localhost:9200', "gmohandass")
+    es_url = 'http://34.230.32.193:9200'
+    author = "gmohandass"
+    my_es = ESClient(es_url, author)
     my_es.publish_doc("https://www.freelancer.com/projects/graphic-design/build-website-15161743/", "freelancer")
